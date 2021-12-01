3a7acafebb863ba251af07a27990d4cede0fd024
jvwasquevite <jvwasquevite@gmail.com>
2021-12-01 14:54:50 -0300

    feat: installed and configured websocket

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,9 +1,24 @@
 import 'dotenv/config'
 import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 
 import { router } from './routes'
 
 const app = express()
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
 
 app.use(express.json())
 app.use(router)
@@ -16,8 +31,7 @@ app.get('/github', (request, response) => {
 
 app.get('/signin/callback', (request, response) => {
   const { code } = request.query
-
   return response.json(code)
 })
 
-app.listen(4000, () => console.log(`Server is running on port 4000`))
+export { serverHttp, io }
